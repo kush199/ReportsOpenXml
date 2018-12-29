28742f50e517a1ef4999177bf452ee397f9aba72
kumar shubhendu <livelifekush@gmail.com>
2018-12-29 19:34:35 +0530

    First commit for porject Openxml

---
+++ ReportsOpenxml/Program.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using OpenXmlPowerTools;
+
+namespace ReportsOpenxml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string PortraitPath = "D://Portrait.docx";
+            string LandscapePath = "D://Lanscape.docx";
+            CreatePortraitDocumnet(PortraitPath);
+            CreateLanscapeDocumnet(LandscapePath);          
+            MergeDoc(LandscapePath, PortraitPath);
+           
+        }
+
+        private static void CreateLanscapeDocumnet(string LandscapePath)
+        {
+            using (WordprocessingDocument wordDocument =
+             WordprocessingDocument.Create(LandscapePath, WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();   
+                Body body = mainPart.Document.AppendChild(new Body());
+                SectionProperties sectionProp = new SectionProperties(new PageSize() { Width = (UInt32Value)15840U, Height = (UInt32Value)12240U, Orient = PageOrientationValues.Landscape });
+                Paragraph par1 = new Paragraph();
+                Run run = new Run();
+                run.AppendChild(new Text("Lanscape"));
+                par1.Append(run);
+                body.Append(par1);
+                body.Append(sectionProp);
+
+                // Delete the existing header and footer parts
+                mainPart.DeleteParts(mainPart.HeaderParts);
+                mainPart.DeleteParts(mainPart.FooterParts);
+
+                // Create a new header and footer part
+                HeaderPart headerPart = mainPart.AddNewPart<HeaderPart>();
+                FooterPart footerPart = mainPart.AddNewPart<FooterPart>();
+
+                // Get Id of the headerPart and footer parts
+                string headerPartId = mainPart.GetIdOfPart(headerPart);
+                string footerPartId = mainPart.GetIdOfPart(footerPart);
+
+                GenerateHeaderPartContent1(headerPart);
+
+                GenerateFooterPartContent1(footerPart);
+                IEnumerable<SectionProperties> sections = mainPart.Document.Body.Elements<SectionProperties>();
+
+                foreach (var section in sections)
+                {
+                    // Delete existing references to headers and footers
+                    section.RemoveAllChildren<HeaderReference>();
+                    section.RemoveAllChildren<FooterReference>();
+
+                    // Create the new header and footer reference node
+                    section.PrependChild<HeaderReference>(new HeaderReference() { Id = headerPartId });
+                    section.PrependChild<FooterReference>(new FooterReference() { Id = footerPartId });
+                }
+               
+
+            }
+        }
+
+        private static void CreatePortraitDocumnet(string PortraitPath)
+        {
+            using (WordprocessingDocument wordDocument =
+             WordprocessingDocument.Create(PortraitPath, WordprocessingDocumentType.Document))
+            {
+                // Add a main document part. 
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure and add some text.
+                mainPart.Document = new Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+                Paragraph para = new Paragraph();
+              
+                SectionProperties sectionProperties = new SectionProperties(new PageSize() {  Orient = PageOrientationValues.Portrait });              
+                Run run = new Run();
+                run.AppendChild(new Text("Create text in body - PortraitPath"));
+                para.Append(run);
+                
+                body.Append(para);
+                body.Append(sectionProperties);
+
+                // Delete the existing header and footer parts
+                mainPart.DeleteParts(mainPart.HeaderParts);
+                mainPart.DeleteParts(mainPart.FooterParts);
+
+                // Create a new header and footer part
+                HeaderPart headerPart = mainPart.AddNewPart<HeaderPart>();
+                FooterPart footerPart = mainPart.AddNewPart<FooterPart>();
+
+                // Get Id of the headerPart and footer parts
+                string headerPartId = mainPart.GetIdOfPart(headerPart);
+                string footerPartId = mainPart.GetIdOfPart(footerPart);
+
+                GenerateHeaderPartContent(headerPart);
+
+                GenerateFooterPartContent(footerPart);
+                IEnumerable<SectionProperties> sections = mainPart.Document.Body.Elements<SectionProperties>();
+
+                foreach (var section in sections)
+                {
+                    // Delete existing references to headers and footers
+                    section.RemoveAllChildren<HeaderReference>();
+                    section.RemoveAllChildren<FooterReference>();
+
+                    // Create the new header and footer reference node
+                    section.PrependChild<HeaderReference>(new HeaderReference() { Id = headerPartId });
+                    section.PrependChild<FooterReference>(new FooterReference() { Id = footerPartId });
+                }
+
+                // body.Append(createHeader());
+                // body.Append(CreateFooter());
+            }
+        }
+
+        public static void MergeDoc(string Source, string destination)
+        {
+            List<string> docList = new List<string>();
+            string path1 = "D://New folder//Doc1.docx";
+            string path2 = "D://New folder//Doc2.docx";
+            string path3 = "D://New folder//Doc3.docx";
+            string destinationWord = "D://New folder//FinalResult.docx";
+            docList.Add(path1);
+            docList.Add(path2);
+            docList.Add(path3);
+            List<Source> sources = new List<Source>();
+            sources.Add(new Source(new WmlDocument(Source),true));
+            sources.Add(new Source(new WmlDocument(destination),true));
+            foreach(var element in docList)
+            {
+                sources.Add(new OpenXmlPowerTools.Source(new WmlDocument(element), true));
+            }
+             DocumentBuilder.BuildDocument(sources, destinationWord);
+           
+        }
+
+        private static void GenerateFooterPartContent(FooterPart footerPart)
+        {
+            Footer footer1 = new Footer() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "w14 wp14" } };
+            footer1.AddNamespaceDeclaration("wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas");
+            footer1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            footer1.AddNamespaceDeclaration("o", "urn:schemas-microsoft-com:office:office");
+            footer1.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+            footer1.AddNamespaceDeclaration("m", "http://schemas.openxmlformats.org/officeDocument/2006/math");
+            footer1.AddNamespaceDeclaration("v", "urn:schemas-microsoft-com:vml");
+            footer1.AddNamespaceDeclaration("wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing");
+            footer1.AddNamespaceDeclaration("wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing");
+            footer1.AddNamespaceDeclaration("w10", "urn:schemas-microsoft-com:office:word");
+            footer1.AddNamespaceDeclaration("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+            footer1.AddNamespaceDeclaration("w14", "http://schemas.microsoft.com/office/word/2010/wordml");
+            footer1.AddNamespaceDeclaration("wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup");
+            footer1.AddNamespaceDeclaration("wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk");
+            footer1.AddNamespaceDeclaration("wne", "http://schemas.microsoft.com/office/word/2006/wordml");
+            footer1.AddNamespaceDeclaration("wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape");
+
+            Paragraph paragraph1 = new Paragraph() { RsidParagraphAddition = "00164C17", RsidRunAdditionDefault = "00164C17" };
+
+            ParagraphProperties paragraphProperties1 = new ParagraphProperties();
+            ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Footer" };
+
+            paragraphProperties1.Append(paragraphStyleId1);
+
+            Run run1 = new Run();
+            Text text1 = new Text();
+            text1.Text = "Footer";
+
+            run1.Append(text1);
+
+            paragraph1.Append(paragraphProperties1);
+            paragraph1.Append(run1);
+
+            footer1.Append(paragraph1);
+
+            footerPart.Footer = footer1;
+        }
+
+        private static void GenerateHeaderPartContent(HeaderPart headerPart)
+        {
+            Header header1 = new Header() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "w14 wp14" } };
+            header1.AddNamespaceDeclaration("wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas");
+            header1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            header1.AddNamespaceDeclaration("o", "urn:schemas-microsoft-com:office:office");
+            header1.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+            header1.AddNamespaceDeclaration("m", "http://schemas.openxmlformats.org/officeDocument/2006/math");
+            header1.AddNamespaceDeclaration("v", "urn:schemas-microsoft-com:vml");
+            header1.AddNamespaceDeclaration("wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing");
+            header1.AddNamespaceDeclaration("wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing");
+            header1.AddNamespaceDeclaration("w10", "urn:schemas-microsoft-com:office:word");
+            header1.AddNamespaceDeclaration("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+            header1.AddNamespaceDeclaration("w14", "http://schemas.microsoft.com/office/word/2010/wordml");
+            header1.AddNamespaceDeclaration("wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup");
+            header1.AddNamespaceDeclaration("wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk");
+            header1.AddNamespaceDeclaration("wne", "http://schemas.microsoft.com/office/word/2006/wordml");
+            header1.AddNamespaceDeclaration("wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape");
+
+            Paragraph paragraph1 = new Paragraph() { RsidParagraphAddition = "00164C17", RsidRunAdditionDefault = "00164C17" };
+
+            ParagraphProperties paragraphProperties1 = new ParagraphProperties();
+            ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Header" };
+
+            paragraphProperties1.Append(paragraphStyleId1);
+
+            Run run1 = new Run();
+            Text text1 = new Text();
+            text1.Text = "Header";
+
+            run1.Append(text1);
+
+            paragraph1.Append(paragraphProperties1);
+            paragraph1.Append(run1);
+
+            header1.Append(paragraph1);
+
+            headerPart.Header = header1;
+        }
+
+        private static void GenerateFooterPartContent1(FooterPart footerPart)
+        {
+            Footer footer1 = new Footer() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "w14 wp14" } };
+            footer1.AddNamespaceDeclaration("wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas");
+            footer1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            footer1.AddNamespaceDeclaration("o", "urn:schemas-microsoft-com:office:office");
+            footer1.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+            footer1.AddNamespaceDeclaration("m", "http://schemas.openxmlformats.org/officeDocument/2006/math");
+            footer1.AddNamespaceDeclaration("v", "urn:schemas-microsoft-com:vml");
+            footer1.AddNamespaceDeclaration("wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing");
+            footer1.AddNamespaceDeclaration("wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing");
+            footer1.AddNamespaceDeclaration("w10", "urn:schemas-microsoft-com:office:word");
+            footer1.AddNamespaceDeclaration("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+            footer1.AddNamespaceDeclaration("w14", "http://schemas.microsoft.com/office/word/2010/wordml");
+            footer1.AddNamespaceDeclaration("wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup");
+            footer1.AddNamespaceDeclaration("wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk");
+            footer1.AddNamespaceDeclaration("wne", "http://schemas.microsoft.com/office/word/2006/wordml");
+            footer1.AddNamespaceDeclaration("wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape");
+
+            Paragraph paragraph1 = new Paragraph() { RsidParagraphAddition = "00164C17", RsidRunAdditionDefault = "00164C17" };
+
+            ParagraphProperties paragraphProperties1 = new ParagraphProperties()
+            {
+                Justification = new DocumentFormat.OpenXml.Wordprocessing.Justification()
+                {
+                    Val = DocumentFormat.OpenXml.Wordprocessing.JustificationValues.Center
+                }
+            };
+            ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Footer" };
+
+            paragraphProperties1.Append(paragraphStyleId1);
+
+            Run run1 = new Run();
+            Text text1 = new Text();
+            text1.Text = "Footer";
+
+            run1.Append(text1);
+
+            paragraph1.Append(paragraphProperties1);
+            paragraph1.Append(run1);
+
+            footer1.Append(paragraph1);
+
+            footerPart.Footer = footer1;
+        }
+
+        private static void GenerateHeaderPartContent1(HeaderPart headerPart)
+        {
+            Header header1 = new Header() { MCAttributes = new MarkupCompatibilityAttributes() { Ignorable = "w14 wp14" } };
+            header1.AddNamespaceDeclaration("wpc", "http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas");
+            header1.AddNamespaceDeclaration("mc", "http://schemas.openxmlformats.org/markup-compatibility/2006");
+            header1.AddNamespaceDeclaration("o", "urn:schemas-microsoft-com:office:office");
+            header1.AddNamespaceDeclaration("r", "http://schemas.openxmlformats.org/officeDocument/2006/relationships");
+            header1.AddNamespaceDeclaration("m", "http://schemas.openxmlformats.org/officeDocument/2006/math");
+            header1.AddNamespaceDeclaration("v", "urn:schemas-microsoft-com:vml");
+            header1.AddNamespaceDeclaration("wp14", "http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing");
+            header1.AddNamespaceDeclaration("wp", "http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing");
+            header1.AddNamespaceDeclaration("w10", "urn:schemas-microsoft-com:office:word");
+            header1.AddNamespaceDeclaration("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
+            header1.AddNamespaceDeclaration("w14", "http://schemas.microsoft.com/office/word/2010/wordml");
+            header1.AddNamespaceDeclaration("wpg", "http://schemas.microsoft.com/office/word/2010/wordprocessingGroup");
+            header1.AddNamespaceDeclaration("wpi", "http://schemas.microsoft.com/office/word/2010/wordprocessingInk");
+            header1.AddNamespaceDeclaration("wne", "http://schemas.microsoft.com/office/word/2006/wordml");
+            header1.AddNamespaceDeclaration("wps", "http://schemas.microsoft.com/office/word/2010/wordprocessingShape");
+
+            Paragraph paragraph1 = new Paragraph() { RsidParagraphAddition = "00164C17", RsidRunAdditionDefault = "00164C17" };
+
+            ParagraphProperties paragraphProperties1 = new ParagraphProperties()
+            {
+                Justification = new DocumentFormat.OpenXml.Wordprocessing.Justification()
+                {
+                    Val = DocumentFormat.OpenXml.Wordprocessing.JustificationValues.Center
+                }
+            };
+            ParagraphStyleId paragraphStyleId1 = new ParagraphStyleId() { Val = "Header" };
+
+            paragraphProperties1.Append(paragraphStyleId1);
+
+            Run run1 = new Run();
+            Text text1 = new Text();
+            text1.Text = "Header";
+
+            run1.Append(text1);
+
+            paragraph1.Append(paragraphProperties1);
+            paragraph1.Append(run1);
+
+            header1.Append(paragraph1);
+
+            headerPart.Header = header1;
+        }
+    }
+}
